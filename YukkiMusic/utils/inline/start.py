9e0fc6a639600826e318308d1dd07d9224e38cb0
Pittow <rdem4209@gmail.com>
2024-09-09 09:20:03 +0530

    Update start.py
    Signed-off-by: Pittow <rdem4209@gmail.com>

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -1,17 +1,6 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 from typing import Union
-
 from pyrogram.types import InlineKeyboardButton
-
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from config import SUPPORT_CHANNEL, SUPPORT_GROUP
 from YukkiMusic import app
 
 
@@ -19,82 +8,45 @@ def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
+                text="➕ Add me to Group", url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+        ],
+        [
+            InlineKeyboardButton(text="📢 Channel", url="https://t.me/pittowpiooh"),
+            InlineKeyboardButton(text="💬 Support", url="https://t.me/randomchatschat"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="❓ How to Use", url=f"https://t.me/{app.username}?start=help"
+            ),
+            InlineKeyboardButton(text="⚙️ Settings", callback_data="settings_helper"),
+        ],
+        [
+            InlineKeyboardButton(text="🌐 Language", callback_data="LG"),
         ],
     ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
-            )
     return buttons
 
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
-        [InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]
-    ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
-            )
-    buttons.append(
         [
             InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ]
-    )
-    if GITHUB_REPO and OWNER:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
-            ]
-        )
-    else:
-
-        if GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
-                ]
-            )
-
-        if OWNER:
-            buttons.append(
-                [
-                    InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                ]
-            )
-    buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
+                text="➕ Add me to Group", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="📢 Channel", url="https://t.me/pittowpiooh"),
+            InlineKeyboardButton(text="💬 Support", url="https://t.me/randomchatschat"),
+        ],
+        [
+            InlineKeyboardButton(text="❓ How to Use", url=f"https://t.me/{BOT_USERNAME}?start=help"),
+            InlineKeyboardButton(text="⚙️ Settings", callback_data="settings_helper"),
+        ],
+        [
+            InlineKeyboardButton(text="🌐 Language", callback_data="LG"),
+        ],
+    ]
     return buttons
 
 
@@ -102,9 +54,12 @@ def alive_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
+                text="➕ Add me to Group", url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+        ],
+        [
+            InlineKeyboardButton(text="📢 Channel", url="https://t.me/pittowpiooh"),
+            InlineKeyboardButton(text="💬 Support", url="https://t.me/randomchatschat"),
         ],
     ]
     return buttons
